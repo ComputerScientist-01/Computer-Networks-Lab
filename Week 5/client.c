a8ad50e536ae8eed9e63348bbfe3dd0a756a672d
ComputerScientist-01 <rathi406@gmail.com>
2021-08-23 15:44:43 +0530

    New : client -week 5

---
+++ Week 5/client.c
@@ -0,0 +1,37 @@
+#include "netinet/in.h"
+#include "sys/socket.h"
+#include "stdio.h"
+main()
+{
+    struct sockaddr_in sa, cli;
+    int n, sockfd;
+    int len;
+    char buff[100];
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        printf("\nError in Socket");
+        exit(0);
+    }
+    else
+        printf("\nSocket is Opened ");
+    bzero(&sa, sizeof(sa));
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(5600);
+    if (connect(sockfd, (struct sockaddr *)&sa, sizeof(sa)) < 0)
+    {
+        printf("\nError in connection failed");
+        exit(0);
+    }
+    else
+        printf("\nconnected successfully");
+    if (n = read(sockfd, buff, sizeof(buff)) < 0)
+    {
+        printf("\nError in Reading");
+        exit(0);
+    }
+    else
+    {
+        printf("\nMessage Read \n %s", buff);
+    }
+}
